abfe460b795b8bcd5b0ed7034238f798e4332890
Raji <rajinalan1106@gmail.com>
2025-10-29 17:24:02 +0530

    Commit new file python

---
+++ src/Ex.07_Loop/Lab054_For_While.py
@@ -0,0 +1,18 @@
+for i in range(0,10):
+    if i==5:
+        print("Five")
+    else:
+        print(i)
+
+
+#ERT(Expression and result Table)
+# i/condition/o/p
+# 0/0==5-->False/o/p=0
+# 1/1==5-->False/o/p=1
+# .....
+# 5/5==5-->True/o/p=Five
+# ....
+# 9/9==5-->False/o/p=9
+
+# o/p
+# 1,2,3,4,Five,6,7,8,9
